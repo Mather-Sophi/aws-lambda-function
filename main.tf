3ff82ddfb20162925cbe6a96f393a33875bd1611
Guo <GGuo@C02CT3HXMD6P-MBP.globeandmail.net>
2023-08-10 16:22:44 -0400

    OP-3896:fix:fix typo in ephemeral_storage block

--- main.tf
+++ main.tf
@@ -53,13 +53,14 @@ resource "aws_lambda_function" "lambda" {
     var.layers :
     null
   )
-  ephemeral_storage = {
-    size = var.ephemeral_storage
-  }
   package_type = var.package_type
   // Use empty_function.zip if no other file is specified
   filename = var.package_type == "Zip" ? length(var.filename) > 0 ? var.filename : "${path.module}/files/empty_function.zip" : null
   image_uri = var.package_type == "Image" ? var.image_uri : null
+
+  ephemeral_storage {
+    size = var.ephemeral_storage
+  }
 
   dynamic "environment" {
     for_each = local.env_vars
